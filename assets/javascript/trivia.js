2297efb7e1568a540a022dcf1ef6593f2a0f711e
phild6596 <phild6596@gmail.com>
2017-12-19 12:34:59 -0800

    <JS, HTML, some CSS> Updated the JS to include a for loop for initial questions, added text warning to secs left, hid directions and start button on click. Added images to game.Added reset and start button to HTML.

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,22 +1,62 @@
 $(document).ready(function onStart() {
-    var triviaQuestions = [];
+
+    //Trivia questions
+    var triviaQuestions = [
+        {
+            question: "What was the original name of Guns 'N Roses?",
+            choices: ["Snakepit", "Reckless Road", "Hollywood Rose", "Troubadors"],
+            answer : 2,
+            photo :"assets/images/hollywoodrose.jpg"
+        },
+        {
+            question: "Axl Rose is from?",
+            choices: ["Indiana", "Illinois", "Idaho", "Ohio"],
+            answer: 0,
+            photo: "assets/images/youngaxl.jpg"
+        },
+        {
+            question: "Slash's real name is?",
+            choices: ["William Bailey", "Samuel Hudgens", "Michael Rose", "Saul Hudson"],
+            answer: 3,
+            photo: "assets/images/slash.jpg"
+        },
+        {
+            question: "The song Welcome to the Jungle was written about what city?",
+            choices: ["Los Angeles", "New York", "Chicago", "Detroit"],
+            answer: 0,
+            photo: "assets/images/losangeles.jpg"
+        }
+        
+    ];
+    //Declared variables
     var correct = 0;
     var incorrect = 0;
     var time = 20;
+    var running = false;
     var intervalId;
     var clockRunning = false;
+    var qHolder = [];
 
+$("#play-again").hide();
 
+for (i = 0; i < triviaQuestions.length; i++) {
+    var holder = triviaQuestions[i];
+    console.log(holder);
+}
 
-reset();
-function reset() {
-    //$(".timer").on("click")
+//main game start and reset
+$(".start-button").on("click", function reset() {
+    $("p").hide();
+    $(".start-button").hide();
     function timerRun() {
+        if (!running){
         intervalId = setInterval(decrement, 1000);
+        running = true;
+        }
     }
     function decrement() {
         time--;
-        $(".timer").html("<h3>" + time + "<h3");
+        $(".timer").html("<h3>You have "+ time + " seconds left!<h3");
         if (time === 0) {
             stop();
         }
@@ -26,5 +66,5 @@ function reset() {
     }
     timerRun();
 }
-
+)
 });
